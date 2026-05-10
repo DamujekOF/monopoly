63252502ea08be55514810c35f26bded47e453c8
Damujek <dkmam909@gmail.com>
2026-05-10 15:35:30 +0200

    Update kafelki.py

--- kafelki.py
+++ kafelki.py
@@ -1,10 +1,65 @@
 kafelki = {
-    i: {
-        "koszt": 200,
-        "owner": None,
-        "nasprzedaz": True,
-        "poziom": 0,
-        "czynsz": 50,
-    }
-    for i in range(32)
+    0:  {"skrot": "START ", "pelnanazwa": "Start", "kolor": "special", "koszt": 0, "owner": None, "nasprzedaz": False, "poziom": 0, "czynsz": 0},
+
+    1:  {"skrot": "BROWN1", "pelnanazwa": "Brązowe 1", "kolor": "brown", "koszt": 60, "owner": None, "nasprzedaz": True, "poziom": 0, "czynsz": 2},
+
+    2:  {"skrot": "CHEST ", "pelnanazwa": "Kasa Społeczna", "kolor": "special", "koszt": 0, "owner": None, "nasprzedaz": False, "poziom": 0, "czynsz": 0},
+
+    3:  {"skrot": "BROWN2", "pelnanazwa": "Brązowe 2", "kolor": "brown", "koszt": 60, "owner": None, "nasprzedaz": True, "poziom": 0, "czynsz": 4},
+
+    4:  {"skrot": "TAX ", "pelnanazwa": "Podatek", "kolor": "special", "koszt": 0, "owner": None, "nasprzedaz": False, "poziom": 0, "czynsz": 0},
+
+    5:  {"skrot": "RAIL1 ", "pelnanazwa": "Kolej 1", "kolor": "railroad", "koszt": 200, "owner": None, "nasprzedaz": True, "poziom": 0, "czynsz": 25},
+
+    6:  {"skrot": "LBLUE1", "pelnanazwa": "Jasnoniebieskie 1", "kolor": "lightblue", "koszt": 100, "owner": None, "nasprzedaz": True, "poziom": 0, "czynsz": 6},
+
+    7:  {"skrot": "CHANCE", "pelnanazwa": "Szansa", "kolor": "special", "koszt": 0, "owner": None, "nasprzedaz": False, "poziom": 0, "czynsz": 0},
+
+    8:  {"skrot": "JAIL", "pelnanazwa": "Więzienie", "kolor": "special", "koszt": 0, "owner": None, "nasprzedaz": False, "poziom": 0, "czynsz": 0},
+
+    9:  {"skrot": " PINK1", "pelnanazwa": "Różowe 1", "kolor": "pink", "koszt": 140, "owner": None, "nasprzedaz": True, "poziom": 0, "czynsz": 10},
+
+    10: {"skrot": "ELEC", "pelnanazwa": "Elektrownia", "kolor": "utility", "koszt": 150, "owner": None, "nasprzedaz": True, "poziom": 0, "czynsz": 15},
+
+    11: {"skrot": "PINK2 ", "pelnanazwa": "Różowe 2", "kolor": "pink", "koszt": 160, "owner": None, "nasprzedaz": True, "poziom": 0, "czynsz": 12},
+
+    12: {"skrot": "RAIL2 ", "pelnanazwa": "Kolej 2", "kolor": "railroad", "koszt": 200, "owner": None, "nasprzedaz": True, "poziom": 0, "czynsz": 25},
+
+    13: {"skrot": "ORNG1 ", "pelnanazwa": "Pomarańczowe 1", "kolor": "orange", "koszt": 180, "owner": None, "nasprzedaz": True, "poziom": 0, "czynsz": 14},
+
+    14: {"skrot": "CHEST ", "pelnanazwa": "Kasa Społeczna", "kolor": "special", "koszt": 0, "owner": None, "nasprzedaz": False, "poziom": 0, "czynsz": 0},
+
+    15: {"skrot": "ORNG2 ", "pelnanazwa": "Pomarańczowe 2", "kolor": "orange", "koszt": 200, "owner": None, "nasprzedaz": True, "poziom": 0, "czynsz": 16},
+
+    16: {"skrot": "PARKNG", "pelnanazwa": "Darmowy Parking", "kolor": "special", "koszt": 0, "owner": None, "nasprzedaz": False, "poziom": 0, "czynsz": 0},
+
+    17: {"skrot": "RED1", "pelnanazwa": "Czerwone 1", "kolor": "red", "koszt": 220, "owner": None, "nasprzedaz": True, "poziom": 0, "czynsz": 18},
+
+    18: {"skrot": "CHANCE", "pelnanazwa": "Szansa", "kolor": "special", "koszt": 0, "owner": None, "nasprzedaz": False, "poziom": 0, "czynsz": 0},
+
+    19: {"skrot": "RED2", "pelnanazwa": "Czerwone 2", "kolor": "red", "koszt": 240, "owner": None, "nasprzedaz": True, "poziom": 0, "czynsz": 20},
+
+    20: {"skrot": "RAIL3 ", "pelnanazwa": "Kolej 3", "kolor": "railroad", "koszt": 200, "owner": None, "nasprzedaz": True, "poziom": 0, "czynsz": 25},
+
+    21: {"skrot": "YELL1 ", "pelnanazwa": "Żółte 1", "kolor": "yellow", "koszt": 260, "owner": None, "nasprzedaz": True, "poziom": 0, "czynsz": 22},
+
+    22: {"skrot": "WATER ", "pelnanazwa": "Wodociągi", "kolor": "utility", "koszt": 150, "owner": None, "nasprzedaz": True, "poziom": 0, "czynsz": 15},
+
+    23: {"skrot": "YELL2 ", "pelnanazwa": "Żółte 2", "kolor": "yellow", "koszt": 280, "owner": None, "nasprzedaz": True, "poziom": 0, "czynsz": 24},
+
+    24: {"skrot": "GOJAIL", "pelnanazwa": "Idź Do Więzienia", "kolor": "special", "koszt": 0, "owner": None, "nasprzedaz": False, "poziom": 0, "czynsz": 0},
+
+    25: {"skrot": "GREEN1", "pelnanazwa": "Zielone 1", "kolor": "green", "koszt": 300, "owner": None, "nasprzedaz": True, "poziom": 0, "czynsz": 26},
+
+    26: {"skrot": "CHEST ", "pelnanazwa": "Kasa Społeczna", "kolor": "special", "koszt": 0, "owner": None, "nasprzedaz": False, "poziom": 0, "czynsz": 0},
+
+    27: {"skrot": "GREEN2", "pelnanazwa": "Zielone 2", "kolor": "green", "koszt": 320, "owner": None, "nasprzedaz": True, "poziom": 0, "czynsz": 28},
+
+    28: {"skrot": "RAIL4 ", "pelnanazwa": "Kolej 4", "kolor": "railroad", "koszt": 200, "owner": None, "nasprzedaz": True, "poziom": 0, "czynsz": 25},
+
+    29: {"skrot": "CHANCE", "pelnanazwa": "Szansa", "kolor": "special", "koszt": 0, "owner": None, "nasprzedaz": False, "poziom": 0, "czynsz": 0},
+
+    30: {"skrot": "BLUE1 ", "pelnanazwa": "Granatowe 1", "kolor": "blue", "koszt": 350, "owner": None, "nasprzedaz": True, "poziom": 0, "czynsz": 35},
+
+    31: {"skrot": "BLUE2 ", "pelnanazwa": "Granatowe 2", "kolor": "blue", "koszt": 400, "owner": None, "nasprzedaz": True, "poziom": 0, "czynsz": 50},
 }
